b18a11d97f42c561561c5129dece2e98f1ca577d
vandjac <32146550+vandjac@users.noreply.github.com>
2023-10-05 17:51:14 -0400

    Added BLE scanner
    BLE scanner now works in settings page, tested on physical Android device.

--- screens/Settings.js
+++ screens/Settings.js
@@ -1,21 +1,47 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { View, Text, Image, TouchableOpacity, SafeAreaView, Dimensions, StyleSheet } from 'react-native';
-import { NavigationContainer } from '@react-navigation/native';
-import { createMaterialTopTabNavigator } from '@react-navigation/material-top-tabs';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import Ionicons from '@expo/vector-icons/Ionicons';
 import Modal from 'react-native-modal';
 import { globalStyles } from '../styles/global';
+import BLEScanner from './BLEScanner';
+import * as Location from 'expo-location';
 
 export default function SettingsModal({ isVisible, onClose }) {
-    return (
-      <Modal isVisible={isVisible} onBackdropPress={onClose} backdropOpacity={0.5}>
-        <View style={globalStyles.modalContainer}>
-          <TouchableOpacity onPress={onClose} style={globalStyles.closeButton}>
-            <Ionicons name="ios-close-circle" size={36} color="black" />
-          </TouchableOpacity>
-          <Text>Settings Page</Text>
-        </View>
-      </Modal>
-    );
-  }
+  // State to track whether Bluetooth permissions have been requested
+  const [bluetoothPermissionRequested, setBluetoothPermissionRequested] = useState(false);
+
+  // Function to request Bluetooth and Location permissions
+  const requestBluetoothPermission = async () => {
+    let { status: locationStatus } = await Location.requestForegroundPermissionsAsync();
+
+    if (locationStatus !== 'granted') {
+      console.log('Permission to access location was denied');
+      return;
+    }
+
+    // Bluetooth permissions are no longer explicitly requested using Expo
+    // You can use Bluetooth features as needed, assuming the Location permission is granted.
+    
+    console.log('Location permission granted');
+  };
+
+  // useEffect to request Bluetooth permissions only the first time the modal is opened
+  useEffect(() => {
+    if (!bluetoothPermissionRequested) {
+      requestBluetoothPermission();
+      setBluetoothPermissionRequested(true); // Set the flag to true to prevent re-requesting
+    }
+  }, []);
+
+  return (
+    <Modal isVisible={isVisible} onBackdropPress={onClose} backdropOpacity={0.5}>
+      <View style={globalStyles.modalContainer}>
+        <TouchableOpacity onPress={onClose} style={globalStyles.closeButton}>
+          <Ionicons name="ios-close-circle" size={36} color="black" />
+        </TouchableOpacity>
+        <Text>Settings Page</Text>
+        <BLEScanner />
+      </View>
+    </Modal>
+  );
+}
