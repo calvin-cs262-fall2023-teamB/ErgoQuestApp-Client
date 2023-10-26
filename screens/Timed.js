68708f9d11669bd65658be97434ce4d2d9ba5208
BHlathein <dsotmfloyd@gmail.com>
2023-10-25 21:03:22 -0400

    Add settings button

--- screens/Timed.js
+++ screens/Timed.js
@@ -1,16 +1,10 @@
 import React, { useState } from 'react';
-import { View, Text, Image, TouchableOpacity, SafeAreaView, Dimensions, StyleSheet } from 'react-native';
-import { NavigationContainer } from '@react-navigation/native';
-import { createMaterialTopTabNavigator } from '@react-navigation/material-top-tabs';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import Ionicons from '@expo/vector-icons/Ionicons';
-import Modal from 'react-native-modal';
-import { globalStyles } from '../styles/global';
+import { View, Text } from 'react-native';
 
-export default function TimedScreen() {
-    return(
-        <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-            <Text>Timed Page</Text>
-        </View>
-    );
+export default function TimedScreen({ isDarkMode }) {
+  return (
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      <Text style={{ color: isDarkMode ? 'white' : 'black' }}>Timed Page</Text>
+    </View>
+  );
 }
