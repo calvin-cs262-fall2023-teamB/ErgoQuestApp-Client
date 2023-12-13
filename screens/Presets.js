f7b7bba99650323d62591b413623b5111082620d
HenryGold5 <hgoldkuhle@gmail.com>
2023-12-13 01:33:32 -0500

    Presets page

--- screens/Presets.js
+++ screens/Presets.js
@@ -69,12 +69,14 @@ export default function PresetsScreen({ navigation }) {
     const startRename = (id) => {
         // console.log('Rename Preset with ID =', id);
         setSelectedID(id);
+        global.selectedPresetId = id;
         setRenameVisible(renameVisible ? false : true);
     };
 
     const openPresetOptions = (id) => {
         // console.log('Delete Preset with ID =', id);
         setSelectedID(id);
+        global.selectedPresetId = id;
         setPresetOptionsVisible(presetOptionsVisible ? false : true);
     };
 
@@ -113,34 +115,100 @@ export default function PresetsScreen({ navigation }) {
         }
     };
 
-    const createPreset = () => {
+    const createPreset = async () => {
         let largestIndex = 1;
-        for (let i = 0; i < global.presets.length; i++) {
-            if (global.presets[i].id > largestIndex) {
-                largestIndex = global.presets[i].id;
+        let newID;
+
+        if(global.presets.length != 0) {
+            for (let i = 0; i < global.presets.length; i++) {
+                if (global.presets[i].id > largestIndex) {
+                    largestIndex = global.presets[i].id;
+                }
             }
+            newID = largestIndex + 1;
+        } else {
+            newID = 1;
         }
-        const newID = largestIndex + 1;
+
+        //console.log(newID);
+
         if (tempName === "" || tempName == undefined) {
             tempName = "Preset " + newID + "";
         }
-        // console.log("Create preset: " + tempName);
-        let newPresets = [];
-        for (let i = 0; i < global.presets.length; i++) {
-            newPresets.push(global.presets[i]);
+
+        if (global.userData && global.userData.id) {
+            const newPresetData = {
+                name: tempName,
+                userId: global.userData.id
+            };
+
+            try {
+                const newPreset = await savePresetToDatabase(newPresetData);
+                if (newPreset && newPreset.id) {
+                    global.presets.push({
+                        id: newPreset.id, // Spread the newPreset object
+                        name: tempName,
+                        actuatorValues: global.moves,
+                    });
+                    //console.log(global.moves);
+                    //console.log(global.presets);
+                    global.selectedPresetId = newPreset.id;
+                } else {
+                    throw new Error("Failed to create new preset");
+                }
+            } catch (error) {
+                console.error("Error creating preset:", error);
+                // Handle the error, maybe alert the user
+                return;
+            }
+        } else {
+            // console.log("Create preset: " + tempName);
+            let newPresets = [];
+            for (let i = 0; i < global.presets.length; i++) {
+                newPresets.push(global.presets[i]);
+            }
+            // TODO: get actuatorValues from move screen
+            newPresets.push({
+                name: tempName,
+                id: newID,
+                actuatorValues: global.moves,
+            });
+            global.presets = newPresets;
         }
-        // TODO: get actuatorValues from move screen
-        newPresets.push({
-            name: tempName,
-            id: newID,
-            actuatorValues: global.moves,
-        });
+        console.log("Selected Preset ID: ", global.selectedPresetId);
         // newPresets[newPresets.length - 1].actuatorValues.push({id: 1, position: 25}); // 2 ways of saving presets available
-        global.presets = newPresets;
         tempName = undefined;
         setCreateVisible(false);
         setCreateOptionsVisible(false); // just in case
     };
+
+    const savePresetToDatabase = async (data) => {
+        try {
+            const response = await fetch('https://ergoquestapp.azurewebsites.net/presets', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify({
+                    name: data.name,
+                    DBUserID: data.userId
+                }),
+            });
+    
+            if (!response.ok) {
+                const errorResponse = await response.text();
+                throw new Error(`HTTP Error: ${response.status} - ${errorResponse}`);
+            }
+    
+            const responseData = await response.json();
+            console.log("Response from savePresetToDatabase:", responseData);
+            return responseData;
+        } catch (error) {
+            console.error('Error saving preset to database:', error);
+            // Further error handling or user notification
+        }
+    };
+    
 
     const deletePreset = () => {
         if (selectedID === undefined) {
@@ -171,6 +239,7 @@ export default function PresetsScreen({ navigation }) {
 
     const updateActuators = () => {
         const id = selectedID;
+        global.selectedPresetId = id;
         console.log('Activate ID: ', id);
         for (let i = 0; i < global.presets.length; i++) {
             if (global.presets[i].id === id) {
@@ -207,72 +276,72 @@ export default function PresetsScreen({ navigation }) {
     };
 
     // display
-   return (
-  <View style={styles.container}>
-    {/* Presets listed */}
-    <FlatList
-      scrollEnabled={true}
-      contentContainerStyle={{ paddingBottom: 20 }} // Adds padding at the bottom of the list
-      style={styles.pageArea}
-      data={global.presets}
-      extraData={presets} // to help screen refresh when changes made in Move screen
-      renderItem={({ item }) => (
-        <View style={[styles.preset]}>
-          <TouchableOpacity
-            onPress={() => activate(item.id)}
-            onLongPress={() => startRename(item.id)}
-            style={[styles.presetButton, styles.presetButtonLeft]}
-          >
-            <Text style={[styles.presetButtonText]}>{item.name}</Text>
-          </TouchableOpacity>
-          <TouchableOpacity
-            onPress={() => openPresetOptions(item.id)}
-            onLongPress={() => openPresetOptions(item.id)}
-            style={[styles.presetButton, styles.presetButtonRight]}
-          >
-            <View style={styles.iconWrapper}>
-              <Ionicons name="ellipsis-horizontal" size={24} color="black" />
-            </View>
-          </TouchableOpacity>
-        </View>
-            )} >
-    </FlatList>
+    return (
+        <View style={styles.container}>
+            {/* Presets listed */}
+            <FlatList
+                scrollEnabled={true}
+                contentContainerStyle={{ paddingBottom: 20 }} // Adds padding at the bottom of the list
+                style={styles.pageArea}
+                data={global.presets}
+                extraData={presets} // to help screen refresh when changes made in Move screen
+                renderItem={({ item }) => (
+                    <View style={[styles.preset]}>
+                        <TouchableOpacity
+                            onPress={() => activate(item.id)}
+                            onLongPress={() => startRename(item.id)}
+                            style={[styles.presetButton, styles.presetButtonLeft]}
+                        >
+                            <Text style={[styles.presetButtonText]}>{item.name}</Text>
+                        </TouchableOpacity>
+                        <TouchableOpacity
+                            onPress={() => openPresetOptions(item.id)}
+                            onLongPress={() => openPresetOptions(item.id)}
+                            style={[styles.presetButton, styles.presetButtonRight]}
+                        >
+                            <View style={styles.iconWrapper}>
+                                <Ionicons name="ellipsis-horizontal" size={24} color="black" />
+                            </View>
+                        </TouchableOpacity>
+                    </View>
+                )} >
+            </FlatList>
 
-    {/* "Add Preset" button at bottom of screen */}
-    <View style={[styles.pageArea, styles.pageBottom]}>
-        <Pressable
-            onPress={onCreateLongPress}
-            onLongPress={onCreateLongPress}
-            style={({ pressed }) => [
-                styles.addButton,
-                {
-                    backgroundColor: pressed ? 'lightgray' : 'white',
-                    borderRadius: 15, 
-                },
-                (updating >= 0) ? styles.hide : styles.show
-            ]}
-        >
-            <Text style={styles.buttonText}>
-                Create New Preset
-            </Text>
-        </Pressable>
-        {/* Update preset */}
-        <Pressable
-            onPress={finishUpdating}
-            style={({ pressed }) => [
-                styles.addButton,
-                {
-                    backgroundColor: pressed ? 'lightgray' : 'white',
-                    borderRadius: 15, 
-                },
-                (updating < 0) ? styles.hide : styles.show
-            ]}
-        >
-            <Text style={styles.buttonText}>
-                Save over {updating >= 0 ? global.presets[updating].name : "Selected Preset"}?
-            </Text>
-        </Pressable>
-        </View>
+            {/* "Add Preset" button at bottom of screen */}
+            <View style={[styles.pageArea, styles.pageBottom]}>
+                <Pressable
+                    onPress={onCreateLongPress}
+                    onLongPress={onCreateLongPress}
+                    style={({ pressed }) => [
+                        styles.addButton,
+                        {
+                            backgroundColor: pressed ? 'lightgray' : 'white',
+                            borderRadius: 15,
+                        },
+                        (updating >= 0) ? styles.hide : styles.show
+                    ]}
+                >
+                    <Text style={styles.buttonText}>
+                        Create New Preset
+                    </Text>
+                </Pressable>
+                {/* Update preset */}
+                <Pressable
+                    onPress={finishUpdating}
+                    style={({ pressed }) => [
+                        styles.addButton,
+                        {
+                            backgroundColor: pressed ? 'lightgray' : 'white',
+                            borderRadius: 15,
+                        },
+                        (updating < 0) ? styles.hide : styles.show
+                    ]}
+                >
+                    <Text style={styles.buttonText}>
+                        Save over {updating >= 0 ? global.presets[updating].name : "Selected Preset"}?
+                    </Text>
+                </Pressable>
+            </View>
             {/* Dialogs */}
             <View style={globalStyles.container}>
                 {/* RENAME */}
@@ -387,17 +456,17 @@ const styles = StyleSheet.create({
         justifyContent: "center",
         fontSize: 24,
         padding: "2%",
-        textAlignVertical: 'center', 
+        textAlignVertical: 'center',
     },
     pageBottom: {
         height: "10%",
     },
     addButton: {
-        width: "100%", 
-        height: 70, 
-        backgroundColor: 'white', 
-        justifyContent: 'center', 
-        alignItems: 'center', 
+        width: "100%",
+        height: 70,
+        backgroundColor: 'white',
+        justifyContent: 'center',
+        alignItems: 'center',
     },
 
     buttonText: {
@@ -408,7 +477,7 @@ const styles = StyleSheet.create({
         padding: "5%",
         width: "100%",
         height: "100%",
-        
+
     },
     hide: {
         display: "none",
@@ -423,5 +492,5 @@ const styles = StyleSheet.create({
         backgroundColor: 'white',
         justifyContent: 'center',
         alignItems: 'center',
-      },
+    },
 });
